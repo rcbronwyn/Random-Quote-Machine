30539d0833ba8fc986b6e5d0135f0ed1f5e01fef
rcbronwyn <rcbronwyn@gmail.com>
2017-01-12 21:21:25 -0500

    Array set up
    Array of quote objects.  They still need the additional fields of
    citation and year for each quote.

---
+++ js/quotes.js
@@ -0,0 +1,72 @@
+var quotes = [
+    {
+        quote: 'If you can dream it, you can do it',
+        author: 'Walt Disney',
+        category: 'Inspiriation'
+    },
+    {
+        quote: 'Success is where preparation and opporunity meet.',
+        author: 'Bobby Unser',
+        category: 'Sports'
+    },
+    {
+        quote: 'If you can dream it, you can do it',
+        author: 'Walt Disney'
+    },
+    {
+        quote: 'Behind every great man is a woman rolling her eyes.',
+        author: 'Jim Carey',
+        category: 'Humor'
+
+    },
+    {
+        quote: 'Oh, the weather outside is weather.',
+        author: 'Kunu, Forgetting Sarah Marshall',
+        category: 'Movies'
+    },
+    {
+        quote: 'The supreme art of war is to subdue the enemy without fighting',
+        author: 'Sun Tzu',
+        category: 'War'
+    },
+    {
+        quote: "If it doesn't matter who wins or loses, then why do they keep score?",
+        author: 'Vince Lombardi',
+        category: 'Sports'
+    },
+    {
+        quote: 'The purpose of art is washing the dust of daily life off our souls.',
+        author: 'Pablo Picasso',
+        category: 'Art'
+    },
+    {
+        quote: 'What we think, we become.',
+        author: 'Buddha',
+        category: 'Inspiration'
+    },
+    {
+        quote: 'Expert: a man who makes three correct guesses consecutively',
+        author: 'Laurence J. Peter',
+        category: 'Humor'
+    },
+    {
+        quote: "Discovered by the Germans in 1904, they named it San Diego, which, of cource, in German means a whale's vagina.",
+        author: 'Ron Burgandy - Anchorman',
+        category: 'Movies'
+    },
+    {
+        quote: "America is all about speed.  Hot, nasty, badass speed.",
+        author: 'Eleanor Roosevelt, Taladegha Nights',
+        category: 'Movies'
+    },
+    {
+        quote: "Well, I'm the best there is.  Plain and simple, when I wake up in the morning I piss excellence.",
+        author: 'Ricky Bobby, Taladegha Nights',
+        category: 'Movies'
+    },
+    {
+        quote: "Everything is funnny, as long as it's happening to somebody else.",
+        author: 'Will Rogers',
+        category: 'Humor'
+    }
+]
